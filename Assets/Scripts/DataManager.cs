5f1118c593610642c82cf8644f8563ea47c541d4
Elenquentaro <elenquentaro@gmail.com>
2020-03-15 20:04:28 +0300

    some more bugfixes

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataManager : MonoBehaviour
 {
@@ -37,6 +38,7 @@ public class DataManager : MonoBehaviour
     public static void ResetProgress()
     {
         SaveLoader.Save<Progress>(new Progress());
+        SceneManager.LoadScene(0);
     }
 
     public static void SaveGridState(List<SummandInfo> summandsOnGrid)
